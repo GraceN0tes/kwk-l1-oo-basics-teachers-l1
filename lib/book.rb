0639ad1d85c3b4cfb0b55a18c2ed46f6c82378e4
Grace Park <parkgra3@gmail.com>
2018-07-13 15:27:21 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,22 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  def initialize(brand)
-    @brand = brand
+# book.rb
+# Add you Book class here
+class Book
+  
+  attr_reader :title 
+  attr_accessor :author, :genre, :page_count
+  
+  def initialize(title,author,genre,page_count)
+    @title = title
+    @author = author
+    @genre = genre
+    @page_count = page_count
   end
-
-  def cobble
-    @condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+  
 end
+great_gatsby = Book.new("The Great Gatsby","F Scott Fitzgerald", "Fiction", 180)
+puts great_gatsby.title
+puts great_gatsby.author
+puts great_gatsby.genre
+puts great_gatsby.page_count
+# great_gatsby.author = "F Scott Fitzgerald"
+#puts great_gatsby.author
