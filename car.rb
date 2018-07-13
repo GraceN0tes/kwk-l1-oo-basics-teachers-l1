51bf33e48acbca77815f205749d601ee21a1a883
Grace Park <parkgra3@gmail.com>
2018-07-13 15:18:53 +0000

    Done.

---
+++ car.rb
@@ -0,0 +1,32 @@
+class Car 
+
+  def initialize(model, make, year)
+    @carmodel = model 
+    @carmake = make 
+    @birthyear = year 
+  end 
+ 
+  def model 
+    @carmodel 
+   end 
+ 
+  def make 
+    @carmake
+  end 
+  
+  def year
+    @birthyear 
+  end 
+  
+end
+
+toyota = Car.new("camry", "toyota", "1998")
+toyota.model 
+toyota.make 
+toyota.year
+
+puts "This is my #{toyota.model}. It is a #{toyota.make}, it was made in #{toyota.year}."
+puts toyota.model 
+honda = Car.new("civic", "honda", "2005")
+puts honda 
+puts toyota 
