63e392a84a0bc414edfec55db97b647e50e5cffb
Grace Park <parkgra3@gmail.com>
2018-07-13 15:32:13 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,19 +2,31 @@
 # Add you Book class here
 class Book
   
-  attr_reader :title 
-  attr_accessor :author, :genre, :page_count
-  
-  def initialize(title,author,genre,page_count)
-    @title = title
-    @author = author
-    @genre = genre
-    @page_count = page_count
-  end
-  
-end
-great_gatsby = Book.new("The Great Gatsby","F Scott Fitzgerald", "Fiction", 180)
-puts great_gatsby.title
-puts great_gatsby.author
-puts great_gatsby.genre
-puts great_gatsby.page_count
+  def initialize(title)
+    @title = title 
+end 
+
+  def title 
+    @title 
+end 
+def author=(author)
+  @author = author
+end 
+def author
+  @author
+end 
+def page_count=(num)
+  @page_count = num 
+end 
+def page_count
+  @page_count
+end 
+def genre=(genre)
+  @genre = genre 
+end 
+def genre 
+  @genre 
+end 
+end 
+summer_reading = Book.new("And Then There Were None")
+summer_reading.author = ("Agatha Christie")
